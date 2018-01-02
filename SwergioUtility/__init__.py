84d83c1a1c1975574f7461558fdfa944cdf15520
IshmaGurca <tobias.kraemer@live.de>
2018-01-02 18:58:22 +0100

    init file change

---
+++ SwergioUtility/__init__.py
@@ -0,0 +1,5 @@
+from SwergioUtility.CommunicationEnviroment import *
+from SwergioUtility.ExpertData import *
+from SwergioUtility.MessageUtility import *
+from SwergioUtility.Settings import *
+from SwergioUtility.SocketIOClient import *
